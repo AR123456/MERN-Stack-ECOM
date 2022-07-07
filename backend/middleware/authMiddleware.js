12efe5397dad3055a33fae4d280aeaeb78480737
AR123456 <coloradoz99@gmail.com>
2022-07-07 06:17:51 -0600

    commented auth middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,42 +1,54 @@
-import jwt from 'jsonwebtoken'
-import asyncHandler from 'express-async-handler'
-import User from '../models/userModel.js'
+// validate the token add middleware were we want to protect the route
+import jwt from "jsonwebtoken";
+// use async express handler to handle errors
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token
-
+  // init token
+  let token;
+  //will send this through headers in the
+  //Authorization Key's value as a Bearer token
+  //   console.log(req.headers.authorization);
+  // put in check for Bearer token
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
+    req.headers.authorization.startsWith("Bearer")
   ) {
+    // token found try to decode it
     try {
-      token = req.headers.authorization.split(' ')[1]
-
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-      req.user = await User.findById(decoded.id).select('-password')
-
-      next()
+      //remove the word Bearer from the token
+      token = req.headers.authorization.split(" ")[1];
+      // pass in the token and secret to decode
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // get user id from token and use it to find and return all
+      // the user data but dont send the password
+      req.user = await User.findById(decoded.id).select("-password");
+      //   console.log(req.user);
+      next();
     } catch (error) {
-      console.error(error)
-      res.status(401)
-      throw new Error('Not authorized, token failed')
+      // token could not be decoded
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
     }
   }
 
   if (!token) {
-    res.status(401)
-    throw new Error('Not authorized, no token')
+    // 401 is not authorized
+    res.status(401);
+    throw new Error("Not authorized, no token");
   }
-})
-
+});
+// middleware for admin users
 const admin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
-    next()
+    next();
   } else {
-    res.status(401)
-    throw new Error('Not authorized as an admin')
+    res.status(401);
+    //TODO this error message should not be this specific
+    throw new Error("Not authorized as an admin");
   }
-}
-
-export { protect, admin }
+};
+// use protect in userRoute
+export { protect, admin };
