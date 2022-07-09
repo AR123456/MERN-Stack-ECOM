1261ce2b4858940f66cb77748a0720874b6ae51b
AR123456 <coloradoz99@gmail.com>
2022-07-09 06:35:39 -0600

    comented gen token util

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,9 +1,17 @@
-import jwt from 'jsonwebtoken'
-
+import jwt from "jsonwebtoken";
+// taking in id for user id to add to payload in this token
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  })
-}
+  // return the jwt.sign() method
+  return jwt.sign(
+    //  pass in ID
+    { id },
+    // secret
+    process.env.JWT_SECRET,
+    // third param to set expiration on token
+    {
+      expiresIn: "30d",
+    }
+  );
+};
 
-export default generateToken
+export default generateToken;
