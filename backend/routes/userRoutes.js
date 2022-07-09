217415164381aa884612f45fb4bbee34c7c079d8
AR123456 <coloradoz99@gmail.com>
2022-07-09 06:34:49 -0600

    commented userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 import {
   authUser,
   registerUser,
@@ -9,19 +9,23 @@ import {
   deleteUser,
   getUserById,
   updateUser,
-} from '../controllers/userController.js'
-import { protect, admin } from '../middleware/authMiddleware.js'
+} from "../controllers/userController.js";
+// middle ware to validate token
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route('/').post(registerUser).get(protect, admin, getUsers)
-router.post('/login', authUser)
+router.route("/").post(registerUser).get(protect, admin, getUsers);
+router.post("/login", authUser);
 router
-  .route('/profile')
+  .route("/profile")
   .get(protect, getUserProfile)
-  .put(protect, updateUserProfile)
+  // if we are getting put make it protected and pass in the
+  // updateUserProfile controller
+  .put(protect, updateUserProfile);
 router
-  .route('/:id')
+  .route("/:id")
+  // route for admin to delete a user
   .delete(protect, admin, deleteUser)
   .get(protect, admin, getUserById)
-  .put(protect, admin, updateUser)
+  .put(protect, admin, updateUser);
 
-export default router
+export default router;
