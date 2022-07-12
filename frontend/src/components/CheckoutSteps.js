ffd8a552bb8c88c7e65158fd4bd003967b988296
AR123456 <coloradoz99@gmail.com>
2022-07-12 05:45:47 -0600

    commented checkoutsteps

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -1,43 +1,46 @@
-import React from 'react'
-import { Nav } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+// pass the checkout steps in as props
+// sign in , shipping ,  payment  , place order
 
 const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
   return (
-    <Nav className='justify-content-center mb-4'>
+    <Nav className="justify-content-center mb-4">
+      {/* TODO has step1  been passed in?  */}
       <Nav.Item>
         {step1 ? (
-          <LinkContainer to='/login'>
+          <LinkContainer to="/login">
             <Nav.Link>Sign In</Nav.Link>
           </LinkContainer>
         ) : (
           <Nav.Link disabled>Sign In</Nav.Link>
         )}
       </Nav.Item>
-
+      {/* TODO has step2  been passed in?  */}
       <Nav.Item>
         {step2 ? (
-          <LinkContainer to='/shipping'>
+          <LinkContainer to="/shipping">
             <Nav.Link>Shipping</Nav.Link>
           </LinkContainer>
         ) : (
           <Nav.Link disabled>Shipping</Nav.Link>
         )}
       </Nav.Item>
-
+      {/* TODO has step3  been passed in?  */}
       <Nav.Item>
         {step3 ? (
-          <LinkContainer to='/payment'>
+          <LinkContainer to="/payment">
             <Nav.Link>Payment</Nav.Link>
           </LinkContainer>
         ) : (
           <Nav.Link disabled>Payment</Nav.Link>
         )}
       </Nav.Item>
-
+      {/* TODO has step  been passed in?  */}
       <Nav.Item>
         {step4 ? (
-          <LinkContainer to='/placeorder'>
+          <LinkContainer to="/placeorder">
             <Nav.Link>Place Order</Nav.Link>
           </LinkContainer>
         ) : (
@@ -45,7 +48,7 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
         )}
       </Nav.Item>
     </Nav>
-  )
-}
+  );
+};
 
-export default CheckoutSteps
+export default CheckoutSteps;
