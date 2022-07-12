0a3ddbeea9c2ec606641d7d2b5887754e481fe03
AR123456 <coloradoz99@gmail.com>
2022-07-12 05:46:58 -0600

    comented  meta comp

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import { Alert } from 'react-bootstrap'
-
+import React from "react";
+import { Alert } from "react-bootstrap";
+// children is the text to show
 const Message = ({ variant, children }) => {
-  return <Alert variant={variant}>{children}</Alert>
-}
-
+  return <Alert variant={variant}>{children}</Alert>;
+};
+// set a default for the variant
+//TODO what section is this from
 Message.defaultProps = {
-  variant: 'info',
-}
+  variant: "info",
+};
 
-export default Message
+export default Message;
