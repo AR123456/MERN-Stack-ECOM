361fc3dc8f97774026b658d584726ea3fb152cae
AR123456 <coloradoz99@gmail.com>
2022-07-23 20:28:17 -0600

    show user message if no products found

--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -10,8 +10,7 @@ const Paginate = ({ pages, page, isAdmin = false, keyword = "" }) => {
   // only return if pages are greater than 1
   return (
     pages > 1 && (
-      // TODO fix bug where admin sees admin product list pageination route when he want to
-      // see the users view list of product patination routes  sect 87 Q&A
+      // TODO let user decide how many pages to show 13,87
       <Pagination>
         {/* map through whatever the number of pages is if it is a keyword search use keyword route
         else use the page route  */}
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -49,6 +49,10 @@ const HomeScreen = ({ match }) => {
         <Message variant="danger">{error}</Message>
       ) : (
         <>
+          {/* Done add message product not found if it is not sec 87 q&A */}
+          {products.length === 0 && (
+            <div>No products found with that keyword</div>
+          )}
           <Row>
             {products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -57,7 +61,7 @@ const HomeScreen = ({ match }) => {
             ))}
           </Row>
           {/* pass in pages , keyword if there is on or empty string if no keyword */}
-          {/* TODO add message product not found if it is not sec 87 q&A */}
+
           <Paginate
             pages={pages}
             page={page}
