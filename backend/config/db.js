672113e8b0dd09be5c208588aa95c6390de8fd35
AR123456 <coloradoz99@gmail.com>
2022-07-26 07:19:36 -0600

    remove depricated params from mongoose connection string

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      // TODO verify this, is this still needed ?
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
+      // // TODO verify this, is this still needed ? not with v6
+      // useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useCreateIndex: true,
     });
     // adding some color to console .log
     console.log(`MongoDB Connected: ${conn.connection.host}`);
