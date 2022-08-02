b63c311b2b07aa83fad412293866e276ad5ae092
AR123456 <coloradoz99@gmail.com>
2022-08-02 06:57:57 -0600

    removing some un needed params from mongoose connect

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,16 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      // // TODO verify this, is this still needed ? not with v6
-      // useUnifiedTopology: true,
-      // useNewUrlParser: true,
-      // useCreateIndex: true,
-    });
-    // adding some color to console .log
+    const conn = await mongoose.connect(process.env.MONGO_URI, {});
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    // adding some color to console .log
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
